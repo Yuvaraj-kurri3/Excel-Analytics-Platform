17f30b70cdfe6a6e0e3a26f1e3b1738b925ac483
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2025-07-06 12:20:28 +0530

    Build React and setup server to serve frontend

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
@@ -18,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI)
 // Routes
 app.use('/api/auth', routes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Authentication API');
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
