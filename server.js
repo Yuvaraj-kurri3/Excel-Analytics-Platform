f399b77f15eb5ee3e648baeeb01bcb5f60c57a19
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2025-07-11 18:45:31 +0530

    mongoose to connected mongoose

--- server.js
+++ server.js
@@ -4,49 +4,57 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./backend/routes/auth'); // ✅ Corrected path
 const session = require('express-session');
-const mongooseSession = require('connect-mongodb-session')(session);
+// const mongooseSession = require('connect-mongodb-session')(session);
+const MongoStore = require('connect-mongo');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const app = express();
 
  
 dotenv.config();
-const app = express();
-app.use(cookieParser());
-app.use(express.json());
-
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-
-https://excel-analytics-platform-d8c0.onrender.com
-// app.use(cors({
-//   origin: 'http://localhost:3000', // your frontend origin
-//   credentials: true
-// }));
 app.use(cors({
   origin: 'https://excel-analytics-platform-d8c0.onrender.com', // your frontend origin
   credentials: true
 }));
-const sessions_store = new mongooseSession({
-  uri: process.env.MONGODB_URI, // MongoDB connection string
-  collection: 'MySession', // Collection to store sessions
-});
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+ 
+ 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'yourSecretKey',
   resave: false,
   saveUninitialized: false,
-   store: sessions_store, // <== this line was missing
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    collectionName: 'MySessions',
+      mongoOptions: {
+      ssl: true,
+      tls: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  }),
   cookie: {
-    maxAge: 60 * 60 * 1000, // 1 hour
-    httpOnly: true
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true
   }
 }));
 
+
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('MongoDB error:', err));
+
+ 
 
 app.use('/api/auth', authRoutes);
 
